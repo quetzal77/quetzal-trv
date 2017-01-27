be66a7bdd15cc596fc3124642d3675abae85bcbd
Quetzal Coatl <coatls77@gmail.com>
2017-01-27 08:25:34 +0200

    jsons

--- SCRIPTS/main_onload.js
+++ SCRIPTS/main_onload.js
@@ -1,5 +1,9 @@
 //00.01 Run function on load of World page
 //This is jQuery object that take data from xml and transform them to some collections
 window.onload = function() {
-    $.getJSON( "DATA/globaldb.json", function(data){console.log(data)});
+    $.getJSON( "DATA/areas.json", processMyJson);
 };
+
+var processMyJson = function(result){
+jsonDoc = result;
+}
