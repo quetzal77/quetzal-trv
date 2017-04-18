8703ccfde8861746ff588e98e64cae0f25d3faf5
Svyatoslav Krivsha <sant@pixell.de>
2017-04-18 12:38:35 +0300

    sprite adjustments

--- SCRIPTS/trv_world.js
+++ SCRIPTS/trv_world.js
@@ -38,7 +38,7 @@ function createWorldPageFrontView() {
                 listOfCountries += "<a id='" + country.country_id +
                 "' onclick='javascript:HTML_CreatorOfCountryPage(this.id)' onmouseover='' style='cursor: pointer;'>" +
 //                "<img src='IMG/flag_n_emblem/small_flags/" + country_ident[2].toLowerCase() + ".png' title='" + country.name_full + "' class='countflag' /></a>"
-                "<img id='" + country_ident[2].toLowerCase() + "' src='img_trans.gif' title='" + country.name_full + "' class='countflag' /></a>"
+                "<img id='" + country_ident[2].toLowerCase() + "' src='/img/icon/x.gif' title='" + country.name_full + "' class='countflag' /></a>"
             }
         });
         result += "<div class='my_countries'><div><b>" + cont.name_ru + ":</b> " + setCountriesNumberWithCorrectEnd(countriesPerContinentNumber) +
